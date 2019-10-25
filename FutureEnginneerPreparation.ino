7d2df7cd58bd93c1144fcc1059ac3629925b8f48
yifei liu <arnoliu@arnoliu.onmicrosoft.com>
2019-10-25 22:47:46 +0800

    最终版本， 明天加油！！！！

--- FutureEnginneerPreparation.ino
+++ FutureEnginneerPreparation.ino
@@ -1,10 +1,9 @@
-#include <SoftwareSerial.h>
 #include "UltrasonicSensor.h"//ok
 #include "myLCD.h"
-#include "Arduino.h"
-#include <LiquidCrystal.h>
-#include <Wire.h>
-#include <DFRobot_Servo.h>
+#include "Arduino.h"//basic h
+#include <LiquidCrystal.h>//lcd basic
+#include <Wire.h>//i2c basic
+#include <DFRobot_Servo.h>//servo revised
 #include <mu_vision_sensor_uart_interface.h>
 #include <mu_vision_sensor_uart_hw_interface.h>
 #include <mu_vision_sensor_type.h>
@@ -26,8 +25,10 @@
 #define InfraredSensor 28 //红外避障 黑色信号 数字传感器
 #define Servopin 2		//舵机 (黄PMW，红Vcc，棕色接地) 线朝前
 #define SoundSensorpin A6   //音量传感器
-#define VISION_TYPE VISION_COLOR_RECOGNITION//VISION_TRAFFIC_CARD_DETECT 
-#define Findcolor
+#define vision_type VISION_COLOR_DETECT
+//VISION_TRAFFIC_CARD_DETECT		VISION_COLOR_DETECT(只能识别一种颜色)  VISION_COLOR_RECOGNITION
+
+
 //#define I2C_MODE
 #define SERIAL_MODE
 #define MU_ADDRESS    0x60
@@ -35,25 +36,27 @@
 int right = 0;
 int left = 180;
 int forward = 90;
-
+#pragma region 对象声明区 
 Servo servo;
 SoundSenor soundsenor(SoundSensorpin);
 SuperSoundSenor sss(UltrasonicSensor);
 myLCD lcd;
-//mu mymu(VISION_COLOR_DETECT);
-//int pos=0;
 LED ledg(14);
 LED ledb(15);
 Infrared infrared(InfraredSensor);
 Button btnWhite(41);
-SoftwareSerial myserial(21, 20);
 MuVisionSensor Mu(MU_ADDRESS);
+#pragma endregion
 int times = 0;
 int btnCounter = 0;
 int btnCounter2 = 0;
 bool isBlocked;
 bool ledgStat;
 bool ledbStat;
+enum color
+{
+	red,green,yellow,white,black,blue
+};
 
 void setup()
 {
@@ -86,21 +89,32 @@ void setup()
 	}
 
 #pragma endregion
-
+#pragma region 初始化舵机
 	while (!servo.attached()) { servo.attach(Servopin); }
 	Serial.println("Servo initialized");
+#pragma endregion
+
+
 	delay(100);
 	//while (!= MU_OK) {delay(20);};
-	while (Mu.SensorSetDefault() != MU_OK) { delay(20); };
-	while (Mu.CameraSetAwb(kAutoWhiteBalance) != MU_OK) { delay(20); };
-
-	Mu.VisionBegin(VISION_COLOR_RECOGNITION);
-	while (Mu.LedSetColor(kLed1, kLedWhite, kLedWhite, 1) != MU_OK) { delay(20); };
-	Mu.LedSetColor(kLed2, kLedBlue, kLedClose, 1);
-	Mu.write(VISION_COLOR_RECOGNITION, kXValue, 50);                  // set detect region center x value(0~100)
-	Mu.write(VISION_COLOR_RECOGNITION, kYValue, 50);                  // set detect region center y value(0~100)
-	Mu.write(VISION_COLOR_RECOGNITION, kWidthValue, 5);               // set detect region center width value(0~100)
-	Mu.write(VISION_COLOR_RECOGNITION, kHeightValue, 5);
+#pragma region 配置MU
+	while (Mu.SensorSetDefault() != MU_OK) { delay(20); };//重置设定
+	while (Mu.CameraSetAwb(kAutoWhiteBalance) != MU_OK) { delay(20); };//自动白平衡
+	while (Mu.LedSetColor(kLed1, kLedWhite, kLedWhite, 7) != MU_OK) { delay(20); };
+	Mu.LedSetColor(kLed2, kLedBlue, kLedClose, 7);
+	//不同模式
+	switch (vision_type)
+	{
+	case VISION_COLOR_RECOGNITION:
+		initColorReg();
+		break;
+	case VISION_COLOR_DETECT:
+		initColorDetect(red);
+		break;
+	default:
+		break;
+	}
+#pragma endregion
 
 }
 
@@ -115,6 +129,56 @@ void loop()
 	//Serial.println(sss.sssdistance());
 	/*waitForHuman();*/
 	waitForTarget();
+	colorDetect();
+	Serial.print(++btnCounter);
+	delay(100);
+}
+void initColorReg()
+{
+	Mu.VisionBegin(VISION_COLOR_RECOGNITION);
+	Mu.write(VISION_COLOR_RECOGNITION, kXValue, 50);
+	Mu.write(VISION_COLOR_RECOGNITION, kYValue, 50);
+	Mu.write(VISION_COLOR_RECOGNITION, kWidthValue, 5);
+	Mu.write(VISION_COLOR_RECOGNITION, kHeightValue, 5);
+}
+void initColorDetect(int color)
+{	
+	Mu.VisionBegin(VISION_COLOR_DETECT);
+	int temp = 0;
+	switch (color)
+	{
+	case blue:
+		Mu.write(vision_type, kLabel, MU_COLOR_BLUE);
+		break;
+	case red:
+		Mu.write(vision_type, kLabel, MU_COLOR_RED);
+		break;
+	case yellow:
+		Mu.write(vision_type, kLabel, MU_COLOR_YELLOW);
+		break;
+	case green:
+		Mu.write(vision_type, kLabel, MU_COLOR_GREEN);
+		break;
+	case white:
+		Mu.write(vision_type, kLabel, MU_COLOR_WHITE);
+		break;
+	case black:
+		Mu.write(vision_type, kLabel, MU_COLOR_BLACK);
+		break;
+	default:
+		Serial.println("ColorParInputError!!");
+		break;
+	}
+}
+void waitForTarget()
+{
+	Serial.println("Waiting");
+	while (!Mu.GetValue(vision_type, kStatus))
+	{
+	}
+}
+void colorRecongnize()
+{
 	switch (Mu.GetValue(VISION_COLOR_RECOGNITION, kLabel))
 	{
 	case MU_COLOR_BLACK:
@@ -144,22 +208,12 @@ void loop()
 		Serial.println(Mu.GetValue(VISION_COLOR_RECOGNITION, kLabel));
 		break;
 	}
-	Serial.print(++btnCounter);
-	delay(100);
 }
-void FindColor()
+void colorDetect()
 {
-	Mu.LedSetColor(kLed1, kLedWhite, kLedWhite, 1);
-	Mu.LedSetColor(kLed2, kLedBlue, kLedClose, 1);
-	Mu.VisionBegin(VISION_COLOR_DETECT);
-	Mu.write(VISION_COLOR_DETECT, kLabel, MU_COLOR_BLUE);
-}
-void waitForTarget()
-{
-	Serial.println("Waiting");
-	while (!Mu.GetValue(VISION_TYPE, kStatus))
-	{
-	}
+		Serial.print("ColorFind!!");
+	
+	Serial.println(++btnCounter);
 }
 void FindHuman()
 {
@@ -168,17 +222,15 @@ void FindHuman()
 	Mu.LedSetColor(kLed2, kLedRed, kLedWhite, 1);
 
 }
-void waitForHuman()
-{
-	Serial.println("Waiting");
-	while (!Mu.GetValue(VISION_BODY_DETECT, kStatus))
-	{
-	}
-}
+
 //将一个范围内的数映射到另一个范围
 int scaleTo(int original, int min1, int max1, int min2, int max2)
 {
 	return (double)(original - min1) / (max1 - min1) * (max2 - min2) + min2;
+}
+int scale(int input,int min, int max)
+{
+	scaleTo(input,0, 1023, min,max);
 }
 
 
