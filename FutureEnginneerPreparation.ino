acbd1610c4d6c63ef6cb42f320aaa7913ce0a8ec
yifei liu <arnoliu@arnoliu.onmicrosoft.com>
2019-10-26 10:52:10 +0800

    比赛当场

--- FutureEnginneerPreparation.ino
+++ FutureEnginneerPreparation.ino
@@ -20,12 +20,12 @@
 #define LED_RED  5
 #define LED_GREEN 4
 #define BUTTON 3
-#define Beeper 2 //蜂鸣器
+#define Beeper 27 //蜂鸣器
 #define UltrasonicSensor A7 //超声波
 #define InfraredSensor 28 //红外避障 黑色信号 数字传感器
 #define Servopin 2		//舵机 (黄PMW，红Vcc，棕色接地) 线朝前
 #define SoundSensorpin A6   //音量传感器
-#define vision_type VISION_COLOR_DETECT
+#define vision_type VISION_COLOR_RECOGNITION
 //VISION_TRAFFIC_CARD_DETECT		VISION_COLOR_DETECT(只能识别一种颜色)  VISION_COLOR_RECOGNITION
 
 
@@ -46,24 +46,42 @@ myLCD lcd;
 LED ledg(14);
 LED ledb(15);
 Infrared infrared(InfraredSensor);
-Button btnWhite(41);
+Button btnone(40);
+Button btntwo(39);
+Button btnthree(38);
+Button btnconfirm(37);
+
 MuVisionSensor Mu(MU_ADDRESS);
 #pragma endregion
 int times = 0;
-int btnCounter = 0;
+int btnCounter1 = 0;
 int btnCounter2 = 0;
+int btnCounter3 = 0;
+int lastBtn1 = 0;
+int lastBtn2 = 0;
+int lastBtn3 = 0;
+
+
 bool isBlocked;
 bool ledgStat;
 bool ledbStat;
+bool oneflag = false;
+bool threeflag = false;
+bool twoflag = false;
+bool isConfirmed = false;
+int que[10] = { 0,0,0,0,0,0,0,0,0,0 };
+int quee = 0;
+
 enum color
 {
-	red,green,yellow,white,black,blue
+	red, green, yellow, white, black, blue
 };
 
 void setup()
 {
 	Serial.begin(115200);
 	Serial3.begin(9600);
+	pinMode(Beeper, OUTPUT);
 #pragma region 初始化MU视觉传感器
 	IniMU :
 	uint8_t err = MU_ERROR_FAIL;
@@ -78,8 +96,6 @@ void setup()
 		goto IniMU;
 	}
 #elif defined SERIAL_MODE
-	//myserial.begin(9600);
-	// initialized MU on the soft serial port
 	err = Mu.begin(&Serial3);
 #endif // I2c
 	if (err == MU_OK) {
@@ -97,11 +113,8 @@ void setup()
 #pragma endregion
 
 	delay(100);
-	rotateToLeft(servo);
-	delay(1000);
-	rotateToForward(servo);
-	delay(1000);
 	rotateToRight(servo);
+
 	//while (!= MU_OK) {delay(20);};
 #pragma region 配置MU
 	while (Mu.SensorSetDefault() != MU_OK) { delay(20); };//重置设定
@@ -117,6 +130,9 @@ void setup()
 	case VISION_COLOR_DETECT:
 		initColorDetect(red);
 		break;
+	case VISION_BALL_DETECT:
+		initBall();
+		break;
 	default:
 		break;
 	}
@@ -129,15 +145,100 @@ void loop()
 	isBlocked = infrared.isBlocked();
 	ledgStat = ledg.islight();
 	ledbStat = ledb.islight();
-	btnWhite.buttonCounterPress(btnCounter);
+
+	//lastBtn1 = btnCounter1;
+	//lastBtn2 = btnCounter2;
+	//lastBtn3 = btnCounter3;
+
+	//btnone.buttonCounterPress(btnCounter1);
+	//btntwo.buttonCounterPress(btnCounter2);
+	//btnthree.buttonCounterPress(btnCounter3);
+
 #pragma endregion
-	//Serial.println(sss.sssdistance());
-	/*waitForHuman();*/
-	waitForTarget();
-	colorDetect();
-	Serial.print(++btnCounter);
-	delay(100);
+//#pragma region 任意调用
+//	if (btnconfirm.getStatus()) { isConfirmed = true; }
+//
+//	if (lastBtn1 != btnCounter1)
+//	{
+//		que[quee] = 1;
+//		quee++;
+//		lcd.show(1);
+//	}
+//	if (lastBtn2 != btnCounter1) {
+//		que[quee] = 2;
+//		quee++;
+//		lcd.show(2);
+//
+//	}
+//	if (lastBtn3 != btnCounter1) {
+//		que[quee] = 3;
+//		lcd.show(3);
+//
+//		quee++;
+//	}
+//
+//	if (!isConfirmed)goto end;
+//	for (int i = 0; i < 10; i++)
+//	{
+//		switch (que[i])
+//		{
+//		case 1:
+//			goto Task1;
+//			break;
+//		case 2:
+//			//goto Task2;
+//			break;
+//		case 3:
+//			goto Task3;
+//			break;
+//		default:
+//			break;
+//		}
+//	}
+//
+//	return;
+//#pragma endregion
+
+
+#pragma region 第一个任务
+	Task1 :
+	if (oneflag == false)Serial.println(infrared.isBlocked());
+	if (infrared.isBlocked() && oneflag == false)
+	{
+		oneflag = true;
+		rotateToanlge(servo, forward, 135);
+		delay(1000);
+		digitalWrite(Beeper, HIGH);
+		delay(1000);
+		digitalWrite(Beeper, LOW);
+		delay(2000);
+		rotateToRight(servo);
+	}
+#pragma endregion
+#pragma region  第二个任务
+	//Task2 :
+	if (soundsenor.getvolume() >= 200&& oneflag == true)
+	{
+		lcd.show(2);
+		rotateToanlge(servo, forward, 135);
+		delay(2000);
+		rotateToRight(servo);
+	}
+#pragma endregion
+
+#pragma region 第三个任务
+	Task3 :
+	if (threeflag == false && oneflag == true&&twoflag==true)
+	{
+		Serial.println("Task 3");
+		waitForTarget();
+		colorRecongnize();
+	}
+
+#pragma endregion
+
 }
+
 void initColorReg()
 {
 	Mu.VisionBegin(VISION_COLOR_RECOGNITION);
@@ -147,7 +248,7 @@ void initColorReg()
 	Mu.write(VISION_COLOR_RECOGNITION, kHeightValue, 5);
 }
 void initColorDetect(int color)
-{	
+{
 	Mu.VisionBegin(VISION_COLOR_DETECT);
 	int temp = 0;
 	switch (color)
@@ -174,6 +275,10 @@ void initColorDetect(int color)
 		Serial.println("ColorParInputError!!");
 		break;
 	}
+}
+void initBall()
+{
+	Mu.VisionBegin(VISION_BALL_DETECT);
 }
 void waitForTarget()
 {
@@ -207,6 +312,12 @@ void colorRecongnize()
 		break;
 	case MU_COLOR_YELLOW:
 		Serial.println("yellow");
+		threeflag = true;
+		lcd.show(1);
+		rotateToanlge(servo, right, 135);
+		delay(3000);
+		rotateToanlge(servo, 135, 0);
+		lcd.clear();
 		break;
 	default:
 		Serial.print("unknow color type: ");
@@ -216,9 +327,23 @@ void colorRecongnize()
 }
 void colorDetect()
 {
-		Serial.print("ColorFind!!");
-	
-	Serial.println(++btnCounter);
+	Serial.print("ColorFind!!");
+
+}
+void ballDetect()
+{
+	switch (Mu.GetValue(vision_type, kLabel))
+	{
+	case MU_BALL_TABLE_TENNIS:
+		Serial.println("table tennis");
+		break;
+	case MU_BALL_TENNIS:
+		Serial.println("tennis");
+		break;
+	default:
+		Serial.println("unknow ball type");
+		break;
+	}
 }
 void FindHuman()
 {
@@ -228,14 +353,15 @@ void FindHuman()
 
 }
 
+
 //将一个范围内的数映射到另一个范围
 int scaleTo(int original, int min1, int max1, int min2, int max2)
 {
 	return (double)(original - min1) / (max1 - min1) * (max2 - min2) + min2;
 }
-int scale(int input,int min, int max)
+int scale(int input, int min, int max)
 {
-	scaleTo(input,0, 1023, min,max);
+	scaleTo(input, 0, 1023, min, max);
 }
 
 void rotateToForward(Servo& inservo)
@@ -250,14 +376,14 @@ void rotateToRight(Servo& inservo)
 {
 	inservo.angle(right);
 }
-void rotateToanlge(Servo& inservo,int current, int target)
+void rotateToanlge(Servo& inservo, int current, int target)
 {
 	int angle = current;
 	int k = current <= target ? 1 : -1;
-	for (int i = 0; i < abs(current-target); i++)
+	for (int i = 0; i < abs(current - target); i++)
 	{
 		delay(10);
-		angle=angle+k;
+		angle = angle + k;
 		inservo.angle(angle);
 	}
 }
